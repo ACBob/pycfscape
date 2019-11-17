f4fb63aedf832d860da3eceecdf569a5461b8c4b
ACBob <ethbot@hotmail.com>
2019-11-17 21:33:35 +0000

    Add license (GPL) If you don't agree with the crude nature of it, you can remove the ASCII Penis. I'm pretty sure the license allows.

--- main.py
+++ main.py
@@ -1,3 +1,75 @@
+"""
+
+The Giant Penis License (GPL)
+Copyright (c) 2019 ACBob
+
+                ▄▄██▄██▄▄
+              ▄█    █    █▄
+             ▄█           █▄
+             █             █
+            █               █
+            █               █
+            █               █
+            █               █
+             █▄     █     ▄█
+              █    ▄▄▄    █
+              █           █
+              █           █
+              █           █
+              █           █
+              █           █
+              █           █
+              █           █
+              █           █
+              █           █
+              █           █
+              █           █
+              █           █
+        ▄████▄█           █▄████▄
+      ▄█                         █▄
+     █                             █
+    █                               █
+    █                               █
+    █                               █
+    █             ▄▄█▄▄             █
+     █           █     █           █
+      █▄       ▄█       █▄       ▄█
+        █▄▄▄▄▄█           █▄▄▄▄▄█
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+"""
+
 from PyQt5.QtWidgets import qApp, QAction, QMainWindow, QApplication, QWidget, QToolTip, QPushButton, QToolBar, QSplitter, QHBoxLayout
 from PyQt5.QtWidgets import QFileDialog, QTextEdit, QDialog, QGridLayout, QGroupBox, QLabel, QFontDialog, QMessageBox, QStyleFactory, QFrame, QListView
 from PyQt5.QtGui import QIcon, QFont, QStandardItemModel, QStandardItem
